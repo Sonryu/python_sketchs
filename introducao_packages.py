2a74affb1909c2a1567197f7cb9e104c211296fe
Sonryu <watsonvilar@gmail.com>
2025-06-10 09:19:59 -0300

    estudando mais, dessa vez por livro

--- introducao_packages.py
+++ introducao_packages.py
@@ -1,1 +1,20 @@
 #so pra fazer o commit msm, to estudando por livrokkkkkkkkk
+
+from sys import path
+
+import package_aula.package
+
+from package_aula.package import soma
+
+from package_aula import package
+
+from package_aula.package import *
+
+
+
+
+print (soma(1, 2))
+print (package_aula.package.soma(1, 2)) 
+print (package.soma(1, 2))
+print (var)
+print (nova_var)
---
+++ package_aula/package.py
@@ -0,0 +1,22 @@
+import sys
+
+
+
+__all__ = [
+
+    'nova_var',
+    'var',
+    'soma',
+    
+]
+
+var = 'Alguma coisa'
+
+
+nova_var = 'Ok'
+
+
+
+def soma(x, y):
+
+    return x+y
