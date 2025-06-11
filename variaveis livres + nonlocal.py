711c86e5e2341f3a95faf7c7ebf6be69dd95c860
Sonryu <watsonvilar@gmail.com>
2025-06-11 00:41:47 -0300

    aula

--- variaveis livres + nonlocal.py
+++ variaveis livres + nonlocal.py
@@ -14,3 +14,23 @@ dentro2 = fora(20)
 
 print(dentro1)
 print(dentro2)
+
+
+def concatentar(string_inicial):
+    
+    valor_final = string_inicial
+
+    def interna(valor_a_concatenar):
+        nonlocal valor_final
+        valor_final += valor_a_concatenar
+        return valor_final
+    
+    return interna
+
+c = concatentar('a')
+print(c('b'))
+print(c('c'))
+print(c('d'))
+
+final = c
+print(final)
